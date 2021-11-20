179ba665939a5fed81df6f8bc14eacfb14d2c9dd
Stepan <sssstepa452@yandex.ru>
2021-11-20 22:16:26 +0300

    Added citizen playermodels to humanoid charclass

--- schema/charclasses/sh_humanoid.lua
+++ schema/charclasses/sh_humanoid.lua
@@ -1,3 +1,30 @@
 CHARCLASS.DisplayName = "Гуманоид"
 CHARCLASS.DisplayDesc = "Человекоподобное существо (две руки, две ноги, одна голова)"
 CHARCLASS.DisplayColor = Color(150,150,150)
+
+function CHARCLASS:GetValidModels(ply)
+    return {
+        "models/humans/group01/male_01.mdl",
+        "models/humans/group01/male_02.mdl",
+        "models/humans/group01/male_04.mdl",
+        "models/humans/group01/male_05.mdl",
+        "models/humans/group01/male_06.mdl",
+        "models/humans/group01/male_07.mdl",
+        "models/humans/group01/male_08.mdl",
+        "models/humans/group01/male_09.mdl",
+        "models/humans/group02/male_01.mdl",
+        "models/humans/group02/male_03.mdl",
+        "models/humans/group02/male_05.mdl",
+        "models/humans/group02/male_07.mdl",
+        "models/humans/group02/male_09.mdl",
+        "models/humans/group01/female_01.mdl",
+        "models/humans/group01/female_02.mdl",
+        "models/humans/group01/female_03.mdl",
+        "models/humans/group01/female_06.mdl",
+        "models/humans/group01/female_07.mdl",
+        "models/humans/group02/female_01.mdl",
+        "models/humans/group02/female_03.mdl",
+        "models/humans/group02/female_06.mdl",
+        "models/humans/group01/female_04.mdl"
+    }
+end
